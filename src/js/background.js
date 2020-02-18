c12b4b6a5f55897e5ba9052da9be1242b6ac778c
Sherly1001 <50266222+Sherly1001@users.noreply.github.com>
2020-02-18 22:22:25 +0700

    Update background.js

--- src/js/background.js
+++ src/js/background.js
@@ -2,13 +2,6 @@ let fun = function() {
   return {cancel: true};
 };
 
-!function () {
-  for (let url in req.urls) {
-    if (req.urls[url]) urls.push(url);
-  }
-  if (urls.length) chrome.webRequest.onBeforeRequest.addListener(fun, {urls: urls}, ['blocking']);
-}(urls = []);
-
 chrome.runtime.onMessage.addListener(function(req, sender, sendRes) {
   if (req.mes === 'add') {
     urls = [];
