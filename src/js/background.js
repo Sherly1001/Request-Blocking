cd3cbcee4a5623e622b5b346ace798003f95f962
Sherly1001 <Sherly1001@users.noreply.github.com>
2023-06-15 14:43:01 +0700

    update to v3

--- src/js/background.js
+++ src/js/background.js
@@ -1,30 +1,39 @@
-let fun = function() {
-  return {cancel: true};
-};
-
-!function() {
-  chrome.storage.sync.get('block-urls', function(o) {
-    urls = [];
-    for (let url in o['block-urls']) {
-      if (o['block-urls'][url]) urls.push(url);
-    }
-    if (urls.length) chrome.webRequest.onBeforeRequest.addListener(fun, {urls: urls}, ['blocking']);
+async function removeRules() {
+  const rules = await chrome.declarativeNetRequest.getDynamicRules();
+  await chrome.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: rules.map((r) => r.id),
   });
-}();
+}
 
-chrome.runtime.onMessage.addListener(function(req, sender, sendRes) {
-  if (req.mes === 'add') {
-    urls = [];
-    for (let url in req.urls) {
-      if (req.urls[url]) urls.push(url);
-    }
-    if (urls.length) chrome.webRequest.onBeforeRequest.addListener(fun, {urls: urls}, ['blocking']);
-  } else if (req.mes === 'remove') {
-    chrome.webRequest.onBeforeRequest.removeListener(fun);
-    urls = [];
-    for (let url in req.urls) {
-      if (req.urls[url]) urls.push(url);
-    }
-    if (urls.length) chrome.webRequest.onBeforeRequest.addListener(fun, {urls: urls}, ['blocking']);
+async function addRules(urls) {
+  if (urls.length) {
+    chrome.declarativeNetRequest.updateDynamicRules({
+      addRules: urls.map((u, idx) => ({
+        id: idx + 1,
+        action: { type: "block" },
+        condition: {
+          urlFilter: u,
+          resourceTypes: ["main_frame"],
+        },
+      })),
+    });
   }
+}
+
+async function updateRules(urls) {
+  await removeRules();
+  await addRules(urls);
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.get("block-urls", function (o) {
+    let urls = Object.keys(o["block-urls"]).filter((u) => o["block-urls"][u]);
+    updateRules(urls);
+  });
 });
+
+chrome.runtime.onMessage.addListener(async function (req, _sender, _sendRes) {
+  let urls = Object.keys(req.urls).filter((u) => req.urls[u]);
+  updateRules(urls);
+});
+
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,85 +1,96 @@
-$(document).ready(function() {
-  $('.block-urls').on('click', '.rm', function() {
-    $(this).parent().fadeOut(300, function() {
-      if ($(this).parent().children().length < 3) {
-        $(this).parent().hide();
-      }
-      !function(url) {
-        chrome.storage.sync.get('block-urls', function(o) {
-          delete o['block-urls'][url];
-          chrome.storage.sync.set({'block-urls': o['block-urls']});
-          chrome.runtime.sendMessage({mes: 'remove', urls: o['block-urls']});
-        });
-      }($(this).children().first().val());
-      $(this).remove();
-    });
-  });
-
-  $('.block-urls').on('click', '.tg', function() {
-    !function($tg) {
-      chrome.storage.sync.get('block-urls', function(o) {
-        o['block-urls'][$tg.siblings().first().val()] = !tg($tg);
-        chrome.storage.sync.set({'block-urls': o['block-urls']});
-
-        chrome.runtime.sendMessage({mes: 'remove', urls: o['block-urls']});
+$(document).ready(function () {
+  $(".block-urls").on("click", ".rm", function () {
+    $(this)
+      .parent()
+      .fadeOut(300, function () {
+        if ($(this).parent().children().length < 3) {
+          $(this).parent().hide();
+        }
+        !(function (url) {
+          chrome.storage.sync.get("block-urls", function (o) {
+            delete o["block-urls"][url];
+            chrome.storage.sync.set({ "block-urls": o["block-urls"] });
+            chrome.runtime.sendMessage({
+              urls: o["block-urls"],
+            });
+          });
+        })($(this).children().first().val());
+        $(this).remove();
       });
-    }($(this));
   });
 
-  $('.add-url').on('click', '.add', add);
-  $('.add-url').on('keyup', 'input', function(e) {
+  $(".block-urls").on("click", ".tg", function () {
+    !(function ($tg) {
+      chrome.storage.sync.get("block-urls", function (o) {
+        o["block-urls"][$tg.siblings().first().val()] = !tg($tg);
+        chrome.storage.sync.set({ "block-urls": o["block-urls"] });
+        chrome.runtime.sendMessage({ urls: o["block-urls"] });
+      });
+    })($(this));
+  });
+
+  $(".add-url").on("click", ".add", add);
+  $(".add-url").on("keyup", "input", function (e) {
     if (e.keyCode == 13) add();
   });
-  
-  $('footer a').on('click', function(e) {
-    let link = $(this).attr('link');
-    chrome.tabs.create({url: link});
+
+  $("footer a").on("click", function (e) {
+    let link = $(this).attr("link");
+    chrome.tabs.create({ url: link });
   });
-  
-  chrome.storage.sync.get('block-urls', function(o) {
-    for (let url in o['block-urls']) {
-      $('.block-urls').append(`<div><input type="text" value="${url}" readonly><div class="bt tg"><i class="fas fa-toggle-${o['block-urls'][url] ? 'on' : 'off'}"></i></div><div class="bt rm"><i class="fas fa-trash-alt"></i></div></div>`).show();
+
+  chrome.storage.sync.get("block-urls", function (o) {
+    for (let url in o["block-urls"]) {
+      $(".block-urls")
+        .append(
+          `<div><input type="text" value="${url}" readonly><div class="bt tg"><i class="fas fa-toggle-${
+            o["block-urls"][url] ? "on" : "off"
+          }"></i></div><div class="bt rm"><i class="fas fa-trash-alt"></i></div></div>`
+        )
+        .show();
     }
   });
 
-  if ($('.block-urls').children().length < 2) $('.block-urls').hide();
+  if ($(".block-urls").children().length < 2) $(".block-urls").hide();
 
-  $('.add-url input').focus();
+  $(".add-url input").focus();
 
   function add() {
-    let url = $('.add-url input').val();
-    $('.add-url input').val('');
-    let rg = /^(\*|https?|file|ftp):\/\/(\*|.+)\/.+$/gi
-    if (rg.test(url) == false) {
-      alert('invalid url');
-      return;
-    }
-    !function(url) {
-      chrome.storage.sync.get('block-urls', o => {
-        let urls = o['block-urls'] || {};
+    let url = $(".add-url input").val();
+    $(".add-url input").val("");
+    !(function (url) {
+      chrome.storage.sync.get("block-urls", (o) => {
+        let urls = o["block-urls"] || {};
         if (urls[url] === undefined) {
           urls[url] = true;
-          chrome.storage.sync.set({'block-urls': urls});
-          $('.block-urls').append('<div><input type="text" value="' + url + '" readonly><div class="bt tg"><i class="fas fa-toggle-on"></i></div><div class="bt rm"><i class="fas fa-trash-alt"></i></div></div>').show();
-          
-          chrome.runtime.sendMessage({mes: 'add', urls: urls});
+          chrome.storage.sync.set({ "block-urls": urls });
+          $(".block-urls")
+            .append(
+              '<div><input type="text" value="' +
+                url +
+                '" readonly><div class="bt tg"><i class="fas fa-toggle-on"></i></div><div class="bt rm"><i class="fas fa-trash-alt"></i></div></div>'
+            )
+            .show();
+
+          chrome.runtime.sendMessage({ urls });
         } else if (urls[url] === false) {
           tg($(`input[value='${url}']`).siblings().first());
           urls[url] = true;
-          chrome.storage.sync.set({'block-urls': urls});
-          chrome.runtime.sendMessage({mes: 'add', urls: urls});
+          chrome.storage.sync.set({ "block-urls": urls });
+          chrome.runtime.sendMessage({ urls });
         }
       });
-    }(url);
+    })(url);
   }
 
   function tg(e) {
-    if ($(e).children().hasClass('fa-toggle-on')) {
-      $(e).children().removeClass('fa-toggle-on').addClass('fa-toggle-off');
+    if ($(e).children().hasClass("fa-toggle-on")) {
+      $(e).children().removeClass("fa-toggle-on").addClass("fa-toggle-off");
       return true;
     } else {
-      $(e).children().removeClass('fa-toggle-off').addClass('fa-toggle-on');
+      $(e).children().removeClass("fa-toggle-off").addClass("fa-toggle-on");
       return false;
     }
   }
 });
+
